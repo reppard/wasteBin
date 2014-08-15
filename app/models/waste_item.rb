d2eaee1ef8b0ac6b9c6fad211c2f0e5615d0b63c
Mark Walker <mark.walker@manheim.com>
2014-08-14 23:39:52 -0400

    Remove wack characters

--- app/models/waste_item.rb
+++ app/models/waste_item.rb
@@ -1,7 +1,7 @@
 class WasteItem < ActiveRecord::Base
   def self.create_cloud
     weights = [*1..80]
-    content = self.all.collect{ |item| item.content.downcase }
+    content = self.all.collect{ |item| item.content.downcase.gsub(/[^0-9A-Za-z]/, '') }
     word_array = content.join(' ').split(' ')
     word_array.sort_by!{|x| word_array.count(x)}
     word_array = word_array.uniq
